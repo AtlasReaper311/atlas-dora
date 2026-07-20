f856187a354840891aa247eecde7c350b164c89d
Atlas <atlas@atlas-systems.uk>
2026-07-20 19:30:46 +0100

    Align generated module type with public boundary entry

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -9,7 +9,7 @@ interface __BaseEnv_Env {
 }
 declare namespace Cloudflare {
 	interface GlobalProps {
-		mainModule: typeof import("./src/index");
+		mainModule: typeof import("./src/public-boundary");
 	}
 	interface Env extends __BaseEnv_Env {}
 }
